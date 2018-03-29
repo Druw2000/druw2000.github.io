4190445498ecf53076097ec43001fa09394cc9b0
Andruw Earnhardt <aearnhardt2000@gmail.com>
2018-03-29 14:00:06 -0400

    Created CPP folder

---
+++ cpp/roguelike.cpp
@@ -0,0 +1,15 @@
+#include "stdio"
+#include "stdlib"
+#include "iostream"
+#include "math"
+
+int main (int argc, char* argv[]) {
+	// output string
+	char* word;
+	// enumerates through every element
+	for (int i = 0; i < (argv.length / char); i ++) {
+		word[i] = sin(argv[i]);
+	}
+	cout <<  word << endl;
+	return 0;
+}
