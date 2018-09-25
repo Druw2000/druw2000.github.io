25c4ba11928290ae069a8ef6d93f22ea937e8230
Andruw Earnhardt <aearnhardt2000@gmail.com>
2018-09-25 00:15:03 -0400

    Renamed

--- scripts/mv-exec.sh
+++ scripts/mv-exec.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -type f -executable ! \( -path './build/*' -o -name '*.sh' -o -name '*.sample' \) -exec mv {} build \;
+find . -type f -executable ! \( -name 'build/*' -o -name '*.sh' -o -name '*.sample' \) -exec mv {} build \;
