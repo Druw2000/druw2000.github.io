3afbba47cbe8b520185d69940aa1d8a2d7cefa00
Andruw Earnhardt <pepekekson@outlook.com>
2018-12-05 11:07:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+var c = document.getElementById("content");
+var head = document.getElementById("header");
+var body = document.getElementById("body");
+var nav = document.getElementById("nav");
+var code = document.getElementById("code");
+
 var black_google = false;
 function invert() {
-  if (!black_google) {
-    document.getElementById("stylesheet").href="dark.css";
-    black_google = true;
+  if (black_google) {
+    document.style.background = "linear-gradient(#efefef, #2d2d2d)"
+    c.style.backgroundColor = "#efefef"
+    c.style.color = "#2d2d2d"
+    black_google = false;
   }
   else {
-    document.getElementById("stylesheet").href="light.css";
-    black_google = false;
+    document.style.background = "linear-gradient(#2d2d2d, #efefef)"
+    c.style.backgroundColor = "#2d2d2d"
+    c.style.color = "#efefef"
+    black_google = true;
   }
 }
