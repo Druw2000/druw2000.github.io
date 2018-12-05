c47394188150ddc464b5851971aa293eb9dbc2f6
Andruw Earnhardt <pepekekson@outlook.com>
2018-12-05 10:57:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 var black_google = false;
 function invert() {
   if (!black_google) {
-  var c = document.getElementById("content");
-  document.body.style.background = "linear-gradient(#2d2d2d, #f5f5f5)";
-  c.style.color = "#f5f5f5";
-  c.style.backgroundColor = "#2d2d2d";
-  c.style.border = "1px solid #f5f5f5";
-  black_google = true;
+    document.getElementById("stylesheet").href="dark.css";
+    black_google = true;
   }
   else {
-    
+    document.getElementById("stylesheet").href="light.css";
+    black_google = false;
   }
 }
