f3252b0a872bf01919692b158160d5029bd7a539
Andruw Earnhardt <pepekekson@outlook.com>
2019-01-25 14:45:45 -0500

    Update build-tree.sh

--- scripts/build-tree.sh
+++ scripts/build-tree.sh
@@ -2,7 +2,7 @@
 # $1 is the base href, usually "https://druw2000.github.io/src"
 # $2 is the level of search
 # $3 is the output file
-src="https://druw2000.github.io/src"
+src="https://druw2000.github.io"
 if [ $1 = "src" ]; then
 	tree -H $src -L $2 --noreport --charset utf-8 > $3
 else
